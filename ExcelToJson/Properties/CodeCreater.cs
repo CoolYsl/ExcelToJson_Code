2f55953a161681c4419f884e32a638d602f2a903
L <465614842@qq.com>
2019-06-23 10:22:09 +0800

    增加C++的阅读类

--- ExcelToJson/Properties/CodeCreater.cs
+++ ExcelToJson/Properties/CodeCreater.cs
@@ -17,16 +17,18 @@ namespace ExcelToJson.Properties
     {
         public string FilePath;
         public string FileName;
-        public StringBuilder sb = new StringBuilder();
-        public string Suffix;
+        public StringBuilder sbObj = new StringBuilder();
+		public List<string> SheetNames = new List<string>();
+		public string Suffix;
         public ACodeCreater(string filePath, string fileName)
         {
             FilePath = filePath;
             FileName = fileName;
         }
         public const string _namespace = "JsonReadObject";
-        public abstract void CodeCreat(List<DataTable> validSheets);
-        public virtual void SaveToFile(Encoding encoding)
+        public abstract void ObjCodeCreat(List<DataTable> validSheets);
+		public abstract void JsonReaderCodeCreat(List<DataTable> validSheets);
+        public virtual void SaveObjCodeToFile(Encoding encoding)
         {
             try
             {
@@ -41,15 +43,23 @@ namespace ExcelToJson.Properties
                 {
                     Console.WriteLine(FilePath + FileName + Suffix);
                     using (TextWriter writer = new StreamWriter(file, encoding))
-                        writer.Write(sb.ToString());
+                        writer.Write(sbObj.ToString());
                 }
-                sb.Clear();
+                sbObj.Clear();
             }
             catch(Exception ex)
             {
                 throw ex;
             }
         }
+		public void InitSheetName(List<DataTable> validSheets)
+		{ 
+			foreach (var sheet in validSheets)
+			{
+				SheetNames.Add(sheet.TableName);
+			}
+		}
+
     }
 
     class CSharpCodeCreater : ACodeCreater
@@ -59,20 +69,25 @@ namespace ExcelToJson.Properties
         {
             Suffix = ".cs";
         }
-        public override void CodeCreat(List<DataTable> validSheets)
+
+		public override void JsonReaderCodeCreat(List<DataTable> validSheets)
+		{
+		}
+
+		public override void ObjCodeCreat(List<DataTable> validSheets)
         {
             try
             {
-                sb.Clear();
-				sb.AppendLine("using System; \n");
-				sb.AppendLine("namespace " + ACodeCreater._namespace);
-                sb.AppendLine("{");
+                sbObj.Clear();
+				sbObj.AppendLine("using System; \n");
+				sbObj.AppendLine("namespace " + ACodeCreater._namespace);
+                sbObj.AppendLine("{");
 
                 //遍历Sheet
                 foreach (var sheet in validSheets)
                 {
-                    sb.AppendLine("public class " + sheet.TableName);
-                    sb.AppendLine("{");
+                    sbObj.AppendLine("public class " + sheet.TableName);
+                    sbObj.AppendLine("{");
                     int col = 0;
                     //查找字段类型和字段名
                     foreach (DataColumn column in sheet.Columns)
@@ -83,7 +98,7 @@ namespace ExcelToJson.Properties
 						{
 							var newStrs = Notes.Split('\n');
 							foreach (var n in newStrs)
-								sb.AppendLine("//"+ n);
+								sbObj.AppendLine("//"+ n);
 						}
                             
                         if (string.IsNullOrEmpty(fieldName))
@@ -98,11 +113,11 @@ namespace ExcelToJson.Properties
                         }
                         //字符转换为小写
                         fieldType = fieldType.ToLower();
-                        sb.AppendLine("public "+ fieldType + " " + fieldName + ";");
+                        sbObj.AppendLine("public "+ fieldType + " " + fieldName + ";");
                     }
-                    sb.AppendLine("}\n");
+                    sbObj.AppendLine("}\n");
                 }
-                sb.AppendLine("}");
+                sbObj.AppendLine("}");
             }
             catch(Exception ex)
             {
@@ -113,24 +128,186 @@ namespace ExcelToJson.Properties
     }
     class CPPCodeCreater :ACodeCreater
     {
-        public CPPCodeCreater(string filePath, string fileName):base(filePath, fileName)
+		private StringBuilder hsb = new StringBuilder();
+		private StringBuilder cppsb = new StringBuilder();
+		public CPPCodeCreater(string filePath, string fileName):base(filePath, fileName)
         {
             Suffix = ".h";
         }
-        public override void CodeCreat(List<DataTable> validSheets)
+		private void SaveJsonReaderToFile()
+		{
+			Encoding utf8 = new UTF8Encoding(false);
+			try
+			{
+				if (!Directory.Exists(FilePath))
+				{
+					Directory.CreateDirectory(FilePath);
+				}
+				using (FileStream file = new FileStream(FilePath + @"\JsonReader"  + Suffix, FileMode.Create, FileAccess.Write))
+				{
+					using (TextWriter writer = new StreamWriter(file, utf8))
+						writer.Write(hsb.ToString());
+				}
+				using (FileStream file = new FileStream(FilePath + @"\JsonReader.cpp", FileMode.Create, FileAccess.Write))
+				{
+					using (TextWriter writer = new StreamWriter(file, utf8))
+						writer.Write(cppsb.ToString());
+				}
+				hsb.Clear();
+				cppsb.Clear();
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+			}
+		}
+		private void CreatheaderFile()
+		{
+			hsb.Clear();
+			hsb.AppendLine("#pragma once ");
+			hsb.AppendLine("#include \"JsonReadObject.h\"");
+			hsb.AppendLine("#include \"cJson/CJsonObject.hpp\"");
+			hsb.AppendLine("# include <vector>");
+			hsb.AppendLine("# include <fstream>");
+			hsb.AppendLine("using namespace JsonReadObject;");
+			hsb.AppendLine("class JsonReader");
+			hsb.AppendLine("{");
+			hsb.AppendLine("public:");
+			hsb.AppendLine("\tJsonReader();");
+			hsb.AppendLine("\t~JsonReader();");
+			hsb.AppendLine("\tvoid LoadAll();");
+			hsb.AppendLine("private:");
+			foreach(var name in SheetNames)
+			{
+				hsb.AppendLine("\tvoid Load"+name+"();");
+			}
+			hsb.AppendLine("\n");
+			hsb.AppendLine("private:");
+			hsb.AppendLine("\tchar* GetFileStr(const char* fileName);\n");
+			hsb.AppendLine("private:");
+			hsb.AppendLine("\tneb::CJsonObject cJsonObj;");
+			hsb.AppendLine("\tstatic ifstream inFile;");
+			hsb.AppendLine("\tchar buffer[2048];");
+			hsb.AppendLine("private:");
+			foreach (var name in SheetNames)
+			{
+				hsb.AppendLine("\tstd::vector <" + name + "> vec"+name+";");
+			}
+			hsb.AppendLine("};");
+		}
+		private List<string> GetFixedName(List<DataTable> validSheets,string name)
+		{
+			List<string> str = new List<string>();
+			foreach (var sheet in validSheets)
+			{
+				if (name.Equals(sheet.TableName))
+				{
+					int col = 0;
+					foreach (DataColumn column in sheet.Columns)
+					{
+						string fieldName = column.ToString();
+						if (string.IsNullOrEmpty(fieldName))
+						{
+							fieldName = string.Format("col_{0}", col);
+							col++;
+						}
+						str.Add(fieldName);
+					}
+					return str;
+				}
+			}
+			return str;
+		}
+		private void CreatCppFile(List<DataTable> validSheets)
+		{
+			cppsb.Clear();
+			cppsb.AppendLine("#include \"JsonReader.h\"");
+			cppsb.AppendLine("#include \"cJson/CJsonObject.hpp\"");
+			cppsb.AppendLine("# include \"define.h\"");
+			cppsb.AppendLine("#include <fstream>");
+			cppsb.AppendLine("");
+			cppsb.AppendLine("ifstream JsonReader::inFile;");
+			cppsb.AppendLine("const int bufferLen = 2048;");
+			cppsb.AppendLine("JsonReader::JsonReader():buffer{0}");
+			cppsb.AppendLine("{");
+			cppsb.AppendLine("");
+			cppsb.AppendLine("}");
+			cppsb.AppendLine("");
+			cppsb.AppendLine("JsonReader::~JsonReader()");
+			cppsb.AppendLine("{");
+			cppsb.AppendLine("");
+			cppsb.AppendLine("}");
+			cppsb.AppendLine("");
+			cppsb.AppendLine("void JsonReader::LoadAll()");
+			cppsb.AppendLine("{");
+			foreach(var name in SheetNames)
+			{
+				cppsb.AppendLine("\tLoad" + name + "();");
+			}
+			cppsb.AppendLine("}");
+			cppsb.AppendLine("");
+			foreach (var name in SheetNames)
+			{
+				cppsb.AppendLine("void JsonReader::Load" + name + "()");
+				cppsb.AppendLine("{");
+				cppsb.AppendLine("\tOUR_DEBUG((LM_INFO,\"Load" + name + ".json....\"));");
+				cppsb.AppendLine("");
+				cppsb.AppendLine("\tGetFileStr(\"./ Conf / " + name + ".json\");");
+				cppsb.AppendLine("\tcJsonObj.Clear();");
+				cppsb.AppendLine("\tcJsonObj.Parse(buffer);");
+				cppsb.AppendLine("\tint arrlen = cJsonObj.GetArraySize();");
+				cppsb.AppendLine("\tfor (int i=0;i<arrlen;++i)");
+				cppsb.AppendLine("\t{");
+				cppsb.AppendLine("\t\t" + name + " conf;");
+
+				foreach (var fixedName in GetFixedName(validSheets, name))
+				{
+					cppsb.AppendLine("\t\tcJsonObj[i].Get(\"" + fixedName + "\" , conf." + fixedName + ");");
+				}
+				cppsb.AppendLine("\t\tvec" + name + ".push_back(conf);");
+				cppsb.AppendLine("\t}");
+				cppsb.AppendLine("}");
+				cppsb.AppendLine("");
+			}
+			cppsb.AppendLine("char* JsonReader::GetFileStr(const char* fileName)");
+			cppsb.AppendLine("{");
+			cppsb.AppendLine("\tinFile.open(fileName, std::ios::in);");
+			cppsb.AppendLine("\tinFile.seekg(0, std::ios::end);");
+			cppsb.AppendLine("\tint len = inFile.tellg();");
+			cppsb.AppendLine("\tif (len < bufferLen)");
+			cppsb.AppendLine("\t{");
+			cppsb.AppendLine("\t\tinFile.seekg(0, std::ios::beg);");
+			cppsb.AppendLine("\t\tinFile.read(buffer, len);");
+			cppsb.AppendLine("\t}");
+			cppsb.AppendLine("\telse");
+			cppsb.AppendLine("\t{");
+			cppsb.AppendLine("\t\tOUR_DEBUG((LM_INFO, \"Buffer is verflow!!!!!!!!!!%s\", fileName));");
+			cppsb.AppendLine("\t}");
+			cppsb.AppendLine("\tinFile.close();");
+			cppsb.AppendLine("\treturn buffer;");
+			cppsb.AppendLine("}");
+		}
+		public override void JsonReaderCodeCreat(List<DataTable> validSheets)
+		{
+			CreatheaderFile();
+			CreatCppFile(validSheets);
+			SaveJsonReaderToFile();
+		}
+
+		public override void ObjCodeCreat(List<DataTable> validSheets)
         {
             try
             {
-                sb.Clear();
-                sb.AppendLine("namespace " + ACodeCreater._namespace);
-                sb.AppendLine("{");
+                sbObj.Clear();
+                sbObj.AppendLine("namespace " + ACodeCreater._namespace);
+                sbObj.AppendLine("{");
                 
                 //遍历Sheet
                 foreach (var sheet in validSheets)
                 {
-                    sb.AppendLine("class " + sheet.TableName);
-                    sb.AppendLine("{");
-                    sb.AppendLine("public:");
+                    sbObj.AppendLine("class " + sheet.TableName);
+                    sbObj.AppendLine("{");
+                    sbObj.AppendLine("public:");
                     int col = 0;
                     //查找字段类型和字段名
                     foreach (DataColumn column in sheet.Columns)
@@ -141,7 +318,7 @@ namespace ExcelToJson.Properties
 						{
 							var newStrs = Notes.Split('\n');
 							foreach (var n in newStrs)
-								sb.AppendLine("//" + n);
+								sbObj.AppendLine("//" + n);
 						}
 						if (string.IsNullOrEmpty(fieldName))
                         {
@@ -155,11 +332,11 @@ namespace ExcelToJson.Properties
                         }
                         //字符转换为小写
                         fieldType = fieldType.ToLower();
-                        sb.AppendLine(fieldType + " " + fieldName + ";");
+                        sbObj.AppendLine(fieldType + " " + fieldName + ";");
                     }
-                    sb.AppendLine("};\n");
+                    sbObj.AppendLine("};\n");
                 }
-                sb.AppendLine("}");
+                sbObj.AppendLine("}");
             }
             catch
             {
@@ -203,13 +380,18 @@ namespace ExcelToJson.Properties
                 {
                     DataTable sheet = dataSet.Tables[i];
 
-                    if (sheet.Columns.Count > 0 && sheet.Rows.Count > 0)
-                        validSheets.Add(sheet);
+					if (sheet.Columns.Count > 0 && sheet.Rows.Count > 0)
+					{
+						validSheets.Add(sheet);
+					}
+
                 }
                 foreach (var item in CodeCreater)
                 {
-                    item.CodeCreat(validSheets);
-                    item.SaveToFile(encoding);
+					item.InitSheetName(validSheets);
+                    item.ObjCodeCreat(validSheets);
+					item.JsonReaderCodeCreat(validSheets);
+                    item.SaveObjCodeToFile(encoding);
                 }
             }
             catch(Exception ex)
